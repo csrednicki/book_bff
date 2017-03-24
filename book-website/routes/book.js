b242d421cf8b72f0831e15f4672bbc6abdfc8582
Cezary Średnicki <cezary.srednicki@agora.pl>
2017-03-24 15:14:33 +0100

    formatowanie

--- book-website/routes/book.js
+++ book-website/routes/book.js
@@ -3,31 +3,30 @@ var router = express.Router();
 var request = require('request');
 var goodGuy = require('good-guy-http')({maxRetries: 3});
 
+console.log(process.env.NODE_DEBUG);
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('book!');
+router.get('/', function (req, res, next) {
+    res.send('book!');
 });
 
-router.get('/:isbn', function(req, res, next) {
+router.get('/:isbn', function (req, res, next) {
     var isbn = req.params.isbn;
-   
-    goodGuy('https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn).then(function(response) {
-        
+
+    goodGuy('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn).then(function (response) {
+
         return JSON.parse(response.body);
 
-    }).then(function(body) {
+    }).then(function (body) {
         var item = body.items[0].volumeInfo;
-        
-        var tplVars = {
-          title: item.title,
-          thumbnail: item.imageLinks.thumbnail,
-          description: item.description,
-        };
-        
-        res.render('book', tplVars);
-        
-        //res.send('book number '+ isbn + ', '+thumb )
-    });    
+
+        res.render('book', {
+            title: item.title,
+            thumbnail: item.imageLinks.thumbnail,
+            description: item.description
+        });
+
+    });
 });
 
 module.exports = router;
