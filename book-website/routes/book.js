6e9219985b83f7692699c1273a05f383bb600200
Cezary Średnicki <cezary.srednicki@agora.pl>
2017-03-24 15:33:49 +0100

    dodany jsonpath

--- book-website/routes/book.js
+++ book-website/routes/book.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var goodGuy = require('good-guy-http')({maxRetries: 3});
-
-console.log(process.env.NODE_DEBUG);
+var jsonpath = require('jsonpath');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -18,12 +17,11 @@ router.get('/:isbn', function (req, res, next) {
         return JSON.parse(response.body);
 
     }).then(function (body) {
-        var item = body.items[0].volumeInfo;
-
+        
         res.render('book', {
-            title: item.title,
-            thumbnail: item.imageLinks.thumbnail,
-            description: item.description
+            title: jsonpath.query(body, '$..volumeInfo.title'),
+            thumbnail: jsonpath.query(body, '$..volumeInfo..thumbnail'),
+            description: jsonpath.query(body, '$..volumeInfo.description')
         });
 
     });
